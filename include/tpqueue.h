59974f83cf45640ad71f44b67cdf1c5e4426c4f2
Salatio <99189453+Salatio@users.noreply.github.com>
2022-04-27 19:10:12 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,70 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+    struct ITEM {
+        T data;
+        ITEM* next;
+    };
+ public:
+    TPQueue(): head(nullptr), tail(nullptr) {}
+    ~TPQueue() {
+        while (head)
+            pop();
+    }
+    void push(const T& data) {
+        if (tail && head) {
+            if (head->data.prior < data.prior) {
+                ITEM* temp = head;
+                head = create(data);
+                head->next = temp;
+                return;
+            } else {
+                  ITEM* temp = head;
+                  while (temp->next) {
+                      if (temp->next->data.prior < data.prior) {
+                          ITEM* tpr = temp->next;
+                          temp->next = create(data);
+                          temp->next->next = tpr;
+                          return;
+                      }
+                      temp = temp->next;
+                  }
+                  tail->next = create(data);
+                  tail = tail->next;
+            }
+        } else {
+              head = create(data);
+              tail = head;
+        }
+    }
+    T pop() {
+        if (head) {
+            ITEM* temp = head->next;
+            T data = head->data;
+            delete head;
+            head = temp;
+            return data;
+        } else {
+              throw std::string(" Empty ! ");
+        }
+    }
+    void print() const {
+        ITEM* temp = head;
+        while (temp) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+ private:
+    TPQueue::ITEM* create(const T& data) {
+        ITEM* item = new ITEM;
+        item->data = data;
+        item->next = nullptr;
+        return item;
+    }
+    ITEM* head;
+    ITEM* tail;
 };
 
 struct SYM {
